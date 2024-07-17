aaefc252111e19df8fb4ef2b74f75660af44b2d9
sagarrana <ranasagar974@gmail.com>
2024-07-18 01:00:58 +0530

    swagger added

--- Jobs_Api/app.js
+++ Jobs_Api/app.js
@@ -5,11 +5,14 @@ require('express-async-errors')
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limiter')
 const express = require('express')
 const app = express()
 
-// connectDB
+//swagger 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+    // connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middlewares/authentication')
     //routers
@@ -20,18 +23,18 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleWare = require('./middlewares/not-found')
 const errorHandlerMiddleWare = require('./middlewares/error-handler')
 app.set('trust proxy', 1);
-app.use(rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-}))
+
 
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
 app.use(xss())
     //extra packages
-
-//routes
+app.get('/', (req, res) => {
+    res.send('<h1> jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+    //routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
